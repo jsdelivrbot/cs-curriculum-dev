bf1cc86685b60d217ab4eba1f3b64c1eaee616fa
Aaron Hobson <ahobson@ad.corp.google.com>
2018-02-13 21:15:00 -0800

    created card flipping animation

--- grade9/term3/match/script.js
+++ grade9/term3/match/script.js
@@ -1,27 +1,30 @@
 var card;
 
 function preload() {
-  card = loadAnimation("https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/card0000.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/card0001.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/card0002.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/card0003.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/card0004.png");
+
 }
 
 function setup() {
-  createCanvas(800,300);
-  card.frameDelay = 10;
-  card.looping = false;
-  card.playing = false;
+  //createCanvas(800,300);
+  createCanvas(800, 300);
+  card = createSprite(width / 2, height / 2, 171, 158);
+  card.addAnimation("flipping", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/card0000.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/card0001.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/card0002.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/card0003.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/card0004.png");
+  card.animation.frameDelay = 10;
+  card.animation.looping = false;
+  card.animation.playing = false;
+  card.animation.mouseActive = true;
+  card.onMousePressed = function() {
+    this.animation.playing = true;
+    if(this.animation.getFrame() === this.animation.getLastFrame()) {
+      this.animation.goToFrame(0);
+    }
+    else if(this.animation.getFrame() === 0) {
+      this.animation.goToFrame(this.animation.getLastFrame());
+    }
+  };
 }
 
 function draw() {
   background(255,255,255);
-  animation(card, 300, 150);
-}
-
-function mousePressed() {
-  card.playing = true;
-  if(card.getFrame() === card.getLastFrame()) {
-    card.goToFrame(0);
-  }
-  else if(card.getFrame() === 0) {
-    card.goToFrame(card.getLastFrame());
-  }
+  drawSprites();
 }
