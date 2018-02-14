5edb55b32d69eed3dffe76e34f158616b628e5e9
Aaron Hobson <ahobson@ad.corp.google.com>
2018-02-13 20:30:24 -0800

    added match project to 9th grade term 3

--- grade9/term3/match/script.js
+++ grade9/term3/match/script.js
@@ -1,14 +1,27 @@
 var card;
 
 function preload() {
-  card = loadAnimation("assets/card/1.png", "assets/card/2.png", "assets/card/3.png", "assets/card/4.png");
+  card = loadAnimation("https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/1.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/2.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/3.png", "https://codenextcoaches.github.io/cs-curriculum-dev/grade9/term3/match/assets/card/4.png");
 }
 
 function setup() {
   createCanvas(800,300);
+  card.frameDelay = 10;
+  card.looping = false;
+  card.playing = false;
 }
 
 function draw() {
   background(255,255,255);
   animation(card, 300, 150);
+}
+
+function mousePressed() {
+  card.playing = true;
+  if(card.getFrame() === card.getLastFrame()) {
+    card.goToFrame(0);
+  }
+  else if(card.getFrame() === 0) {
+    card.goToFrame(card.getLastFrame());
+  }
 }
