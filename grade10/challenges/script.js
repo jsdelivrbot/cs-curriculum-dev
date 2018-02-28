cc6d6aa226e2b3aae542aaa9b9d0b594110c3ca6
Aaron Hobson <ahobson@ad.corp.google.com>
2018-02-28 14:09:25 -0800

    added coding challenges tests to 10th grade

---
+++ grade10/challenges/script.js
@@ -0,0 +1,32 @@
+/*
+The parameter weekday is true if it is a weekday, and the parameter vacation is
+true if we are on vacation. We sleep in if it is not a weekday or we're on
+vacation. Return true if we sleep in.
+
+sleepIn(false, false) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true
+*/
+
+function sleepIn(weekday, vacation) {
+  if(!weekday || vacation) {
+    return true;
+  }
+  return false;
+}
+
+/*
+Given two int values, return their sum. Unless the two values are the same,
+then return double their sum.
+
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8
+*/
+
+function sumDouble(a, b) {
+  if(a === b) {
+    return (a + b) * 2;
+  }
+  return a + b;
+}
---
+++ grade10/challenges/starter/script.js
@@ -0,0 +1,26 @@
+/*
+The parameter weekday is true if it is a weekday, and the parameter vacation is
+true if we are on vacation. We sleep in if it is not a weekday or we're on
+vacation. Return true if we sleep in.
+
+sleepIn(false, false) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true
+*/
+
+function sleepIn(weekday, vacation) {
+
+}
+
+/*
+Given two int values, return their sum. Unless the two values are the same,
+then return double their sum.
+
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8
+*/
+
+function sumDouble(a, b) {
+
+}
